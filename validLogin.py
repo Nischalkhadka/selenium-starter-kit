37b38856662de2ec0ee2075005e6f30d132a243d
Nischal Khadka <nischalkhadka@lftechnology.com>
2020-06-22 14:07:50 +0545

    s-003:Update readme to organize tests

--- validLogin.py
+++ validLogin.py
@@ -2,17 +2,20 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class PythonOrgSearch(unittest.TestCase):
+class UserLogin(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    def test_search_in_python_org(self):
+    def test_demo_user_valid_login(self):
         driver = self.driver
         driver.get('https://www.phptravels.net/login')
-        self.assertIn("Login", driver.title)
+        
+        # Enter email
         email = driver.find_element_by_name('username')
         email.send_keys("user@phptravels.com")
+        
+        #Enter password and hit enter
         password = driver.find_element_by_name('password')
         password.send_keys("demouser")
         password.send_keys(Keys.RETURN)
